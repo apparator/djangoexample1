03dcd153dfe5b95a9a980f4b6933f0859c3feb2a
Tor Ivar Johansen <tor@torjohansen.no>
2015-03-24 09:31:41 +0100

    refactored student view naming

--- schoolregister/students/urls.py
+++ schoolregister/students/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import patterns, url
 from students import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.index, name='index'),
-    url(r'^(\d+)/$', views.details, name='details'),   
+    url(r'^$', views.student_listing, name='student_listing'),
+    url(r'^(\d+)/$', views.student_details, name='student_details'),   
 )
--- schoolregister/students/views.py
+++ schoolregister/students/views.py
@@ -4,12 +4,12 @@ from django.http import HttpResponse
 from students.models import Student
 
 # Create your views here.
-def index(request):
+def student_listing(request):
     students = Student.objects.all()
     context = {'students': students}
     return render(request, 'students/index.html', context)
 
-def details(request, student_id):
+def student_details(request, student_id):
     student = Student.objects.get(pk=student_id)
     context = {'student': student}
     return render(request, 'students/details.html', context)
