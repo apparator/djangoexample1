a3b2132911228a3c9ae404d2e5c15e48d01527b4
Tor Ivar Johansen <tor@torjohansen.no>
2015-04-14 09:18:14 +0200

    Finished register view with alert message on success

--- schoolregister/useraccounts/views.py
+++ schoolregister/useraccounts/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render
 from django.shortcuts import redirect
+from django.contrib.auth.models import User
 
 def user_login(request):
     context = {}
@@ -18,9 +19,21 @@ def user_login(request):
     # request.method == 'GET':
     return render(request, 'useraccounts/login.html', context)
 
+
 def user_logout(request):
     logout(request)
     return redirect('frontpage')
 
+
 def user_register(request):
-    pass
+    context = {}
+    if request.method == 'POST':
+        user = User()
+        user.first_name = request.POST.get('firstname')
+        user.last_name = request.POST.get('lastname')
+        user.username = request.POST.get('username')
+        user.email = request.POST.get('email')
+        user.set_password(request.POST.get('password'))
+        user.save()
+        context['user_saved_successfully'] = True
+    return render(request, 'useraccounts/register.html', context)
