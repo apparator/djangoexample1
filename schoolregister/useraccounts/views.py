0bc4c9d822fc34a6e03a2d0d02d68a0786939718
Tor Ivar Johansen <tor@torjohansen.no>
2015-04-21 11:17:09 +0200

    Complete register view

--- schoolregister/useraccounts/views.py
+++ schoolregister/useraccounts/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 from django.shortcuts import render
 from django.shortcuts import redirect
 
@@ -23,4 +24,25 @@ def user_logout(request):
     return redirect('frontpage')
 
 def user_register(request):
-    pass
+    context = {}
+    if request.method == "POST":
+        user = User()
+        user.first_name = request.POST.get('firstname')
+        user.last_name = request.POST.get('lastname')
+        user.username = request.POST.get('username')
+        user.email = request.POST.get('email')
+        user.set_password(request.POST.get('password'))
+        user.save()
+        context['user_saved_successfully'] = True
+    return render(request, 'useraccounts/register.html', context)
+
+
+
+
+
+
+
+
+
+
+
